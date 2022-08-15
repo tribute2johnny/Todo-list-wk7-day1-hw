24e994b4dfc5337336ff0b03952733dd756e95e3
John Maclean <tribute2johnny@gmail.com>
2022-08-15 20:35:30 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
+
+function App () {
+  
+  const [chores, setChores] = useState([
+    {name: "Do the Dishes", priority: "High"},
+    {name: "Wash the Car", priority: "Low"},
+    {name: "Cut the Grass", priority: "High"},
+  ])
+
+  const [newChore, setNewChore] = useState("");
+  const [newPriority, setNewPriority] = useState("")
+
+  const choreNodes = chores.map((chore, index) => {
+    return <li key={index}
+    className={chore.priority === "High" ? "priorityHigh" : "priorityLow"}>
+      <span>{chore.name}</span>
+      <span >{chore.priority}</span>
+      
+    </li>
+  })
+
+  const handleChoreInput = (event) => {
+      setNewChore(event.target.value);
+  }
+
+  const handlePriorityInput = (event) => {
+      setNewPriority(event.target.value);
+  }
+
+  const saveNewChore = (event) => {
+    event.preventDefault();
+    const copyChores = [...chores];
+    copyChores.push({name: newChore, priority: newPriority})
+    setChores(copyChores);
+    setNewChore("");
+    }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> To-Do's</h1>
+      
+      <ul>
+        {choreNodes}
+      </ul>
+
+      <form onSubmit={saveNewChore}>
+        <label htmlFor="new-chore">Add a new chore: </label>
+        <input id="new-chore" type="text" value={newChore} onChange={handleChoreInput}/>
+        <>
+        <input type="radio" value="High" name="priority" onChange={handlePriorityInput}/> High
+        <input type="radio" value="Low" name="priority" onChange={handlePriorityInput}/> Low
+        </>
+        <input type="submit" value="Save New Chore" />
+      </form>
+
+
     </div>
-  );
+  )
 }
+
+
+
+
+
 
 export default App;
